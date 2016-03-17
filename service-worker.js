6052dd695413e03851cf1ac5f74983b91ab461e8
Web-Push <cloudlearning777@gmail.com>
2016-03-17 15:20:44 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,21 @@
 
 console.log('WebPushApp2 servicewoker load');
 
+    
+//self.importScripts('https://web-push.github.io/postmessageTest/service-worker.js');
+
+//eval("self.addEventListener('push', function(event) {console.log('Received a push message', event);});");
+self.addEventListener('install', function(event) {
+  console.log('install');
+  var myHeaders = new Headers();
+  myHeaders.set("Access-Control-Allow-Origin", "https://web-push.github.io");
+  var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
     //fetch('https://web-push.github.io/postmessageTest/service-worker.js').then(function(response){
-    fetch('https://html5experts.jp/wp-content/uploads/2014/05/20140523_sw.001.png').then(function(response){
+    fetch('https://html5experts.jp/wp-content/uploads/2014/05/20140523_sw.001.png',
+    ).then(function(response){
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ' + response.status);
       }
@@ -12,9 +25,7 @@ console.log('WebPushApp2 servicewoker load');
         //eval(textdata);
       })
     });
-//self.importScripts('https://web-push.github.io/postmessageTest/service-worker.js');
-
-//eval("self.addEventListener('push', function(event) {console.log('Received a push message', event);});");
+});
 
 self.addEventListener('notificationclick', function(event) {
   console.log('WebPushApp2 On notification click: ', event.notification.tag);
