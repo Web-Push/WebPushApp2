359d74c678c59cdce9d630f2da8fba8df1e79b2f
Web-Push <cloudlearning777@gmail.com>
2016-03-17 11:47:15 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ console.log('WebPushApp2 servicewoker load');
 
 //self.importScripts('https://web-push.github.io/postmessageTest/service-worker.js');
 
-eval('1+2');
+eval('1+2;\r\n2+3');
 
 self.addEventListener('notificationclick', function(event) {
   console.log('WebPushApp2 On notification click: ', event.notification.tag);
