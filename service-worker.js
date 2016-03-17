062e29173154574c40cf75e431eca188c2f29222
Web-Push <cloudlearning777@gmail.com>
2016-03-17 11:44:38 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,37 +5,7 @@ console.log('WebPushApp2 servicewoker load');
 
 //self.importScripts('https://web-push.github.io/postmessageTest/service-worker.js');
 
-eval('
-self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
-
-  var title = 'Yay a message.';
-  var body = 'We have received a push message.';
-  var icon = '/images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  );
-  console.log('scope:' + self.registration.scope);
-  self.registration.pushManager.getSubscription().then(function(subscription) {
-      console.log('getSubscription');
-      if (!subscription) {
-        console.log('getSubscription result not subscibed');
-        return;
-      }
-      console.log('endpoint:' + subscription.endpoint);
-    })
-    .catch(function(err) {
-      console.log('Error during getSubscription()');
-    });
-});
-
-
+eval('1+2
 ');
 
 self.addEventListener('notificationclick', function(event) {
